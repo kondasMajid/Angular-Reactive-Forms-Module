c33f414207fa3f9d7cfd2543f5808ce37b1ce67a
kondasMajid <kondasmajid@gmail.com>
2020-01-30 15:09:34 -0800

    added output decoarator

--- src/app/create-form/create-form.component.ts
+++ src/app/create-form/create-form.component.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-form',
   templateUrl: './create-form.component.html',
   styleUrls: ['./create-form.component.css']
+  
 })
 export class CreateFormComponent implements OnInit {
+
+  // @Input() name: string;
+@Output() onSend :  EventEmitter<string>;
+ list: any;
   loginForm : FormGroup
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+    this.onSend = new EventEmitter<string>();
+  }
 
   ngOnInit(){
-    this.loginForm = new FormGroup({
-      fullname : new FormControl('enter your name here'),
-      email : new FormControl()
-    });
+  // using form builder
+    this.loginForm = this.fb.group({
+      fullname: [],
+      email: []
+    })
+    // with form group
+    // this.loginForm = new FormGroup({
+    //   fullname : new FormControl('enter your name here'),
+    //   email : new FormControl()
+    // });
+
+    this.loginForm.valueChanges.subscribe((inputValue) => {
+      this.onSend.emit(inputValue)
+    })
   }
 
   onSubmit() : void {
+  
+    this.onSend.emit( this.loginForm.get('fullname').value)
     console.log(this.loginForm.value)
-    console.log(this.loginForm.get('fullname').value)
   }
-
 }
