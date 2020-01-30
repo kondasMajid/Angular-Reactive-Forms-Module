658e3476a69659495a847c9b24f3f976fa6e67d6
kondasMajid <kondasmajid@gmail.com>
2020-01-30 08:51:22 -0800

    angular reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateFormComponent } from './create-form/create-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateFormComponent
   ],
   imports: [
     BrowserModule,
